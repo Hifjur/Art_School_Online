5a491d0ad7079016e16b2c884f9e6d3b7918ad9d
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-10-04 01:27:40 +0600

    Navbar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
+const style ={
+    color: 'white',
+    textDecoration: 'none',
+    padding: '20px',
+    
+};
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <NavLink style={style} to ="/home">Home</NavLink>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink style={style} to ="/courses" >Courses</NavLink>
+                            <NavLink style={style} to ="/instructors">Instructors</NavLink>
+                            <NavLink style={style} to ="/aboutus">About us</NavLink>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link to ="/login">Login</Nav.Link>
+                            <Nav.Link to ="/profile">
+                                Profile
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
